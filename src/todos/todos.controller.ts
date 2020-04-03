4d8bb7dd603bfa5d928b65b6ba1ae771be8f3e92
Eduard(server) <noreduard93@gmail.com>
2020-04-03 15:54:30 +0300

    rout todo

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('todos')
 export class TodosController {
-    @Get()
     findAll(): string {
         return 'This action returns all cats';
     }
